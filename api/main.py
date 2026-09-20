d425360a6634d3dea133389e4c740aa3526c65b9
Oluwanifemmi <mizniphemmy@gmail.com>
2026-09-20 13:26:49 +0100

    updated api/main.py

--- api/main.py
+++ api/main.py
@@ -87,3 +87,29 @@ def predict_probability(data: creditfeature):
         'predicted_default': predicted_default,
         'risk_tier': risk_tier
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Feature_engineering.py
+++ src/Feature_engineering.py
@@ -74,8 +74,7 @@ def get_low_iv_columns(
     iv_table = calculate_iv(X, y)
     low_iv_cols = iv_table.loc[iv_table["IV Score"]
                                <= threshold, "Feature"].tolist()
-    logger.info(
-    f"Identified {len(low_iv_cols)} low-IV columns to drop: {low_iv_cols}")
+    logger.info(f"Identified {len(low_iv_cols)} low-IV columns to drop: {low_iv_cols}")
     return low_iv_cols
 
 
